49b59b28807da2b0fb6aef5d28cd08d96f8c9032
Avneesh Sarwate <avneeshsarwate@gmail.com>
2019-07-30 01:13:45 -0400

    found asset bug for faces

--- houseFaces/drawing.js
+++ houseFaces/drawing.js
@@ -1,17 +1,15 @@
 console.log("drawing.js eval");
 
-
 function drawing(){
     let w = p5w;
     let h = p5h;
     let yStep = 24;
     frameInd++;
     // if(frameInd%5 != 0) return;
-    time = (Date.now()/1000);
     for(let y = 0; y < yStep; y++){
-        let xPos = (time*100) % w ;
-        let yPos = h/yStep*y + sin(time * (1+randVals[y]))*yStep;
-        let faceInd = Math.floor(mod(y/yStep, 1) * 6 );
+        let xPos = (frameInd/60*100) % w ;
+        let yPos = h/yStep*y + sin(frameInd/60 * (1+randVals[y]))*yStep;
+        let faceInd = Math.floor(y/yStep * 6 );
         let face = faceImages[faceInd];
         image(face, xPos, yPos, face.width/5, face.height/5);
     }
