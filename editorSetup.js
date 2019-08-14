98c0ea9fc17229d1007bca1b1df58593c72f6a8f
Avneesh Sarwate <avneeshsarwate@gmail.com>
2019-08-14 01:01:22 -0400

    basics of shader error marking working, still minor problems with parsing and getting line numbers right

--- editorSetup.js
+++ editorSetup.js
@@ -1,26 +1,70 @@
 let langTools = ace.require("ace/ext/language_tools");
 // langTools.setCompleters([langTools.snippetCompleter, langTools.keyWordCompleter])
 let editors = [null, null, null, null, null];
-let defaultShaders = ["fs", "fs2"]
+let defaultShaders = ["fs", "fs2"];
+
+let debugging = false;
+
+function setMarkersForShaderError(editorInfo, errorResultString){
+    //TODO - figure out why markers (e.g. line highlights) aren't working properly
+    // while(editorInfo.errorMarkers.length > 0){
+    //     let mark = editorInfo.errorMarkers.pop();
+    //     editorInfo.editor.session.removeMarker(mark);
+    // }
+
+    editorInfo.editor.session.clearAnnotations();
+
+    let lineOffset = headerShader.split("\n").length;
+    let lines = errorResultString.match(/^.*((\r\n|\n|\r)|$)/gm);
+
+    let tAnnotations = [];
+    for (let i = 0; i < lines.length; i++) {
+        let parts = lines[i].split(":");
+        if(i > lineOffset){
+            console.log(lines[i]);
+        }
+        if (parts.length === 5 || parts.length === 6) 
+        {
+            let annotation = {};
+            annotation.row = parseInt(parts[2]) - lineOffset;
+            annotation.text = parts[3] + " : " + parts[4];
+            annotation.type = "error";
+
+            tAnnotations.push(annotation);
+            
+            // let mark = editorInfo.editor.session.addMarker(new Range(annotation.row, 0, annotation.row, 1), "errorHighlight", "fullLine", false);
+            // editorInfo.errorMarkers.push(mark);
+        } 
+    }
+
+    editorInfo.editor.session.setAnnotations(tAnnotations);
+}
+
+debugging = false;
 function initShaderEditor(editorIndex) {
     let editorId = "editor" + editorIndex;
     let editor = ace.edit(editorId);
+
     editor.session.setMode("ace/mode/glsl");
     editor.setTheme("ace/theme/monokaiCustom");
     editor.setDisplayIndentGuides(false);
     editor.setShowPrintMargin(false);
     let editorText = "\n\n\n//editor" + editorIndex;
-    editors[editorIndex] = { editor, id: editorId, text: editorText, visible: false, lang: "frac", timeout: null };
+    editors[editorIndex] = { editor, id: editorId, text: editorText, visible: false, lang: "frag", timeout: null, errorMarkers: []};
     editor.setValue(editorText, -1);
     $("#" + editorId).hide();
+
     editor.session.on("change", (evt) => {
         let editorInfo = editors[editorIndex];
         clearTimeout(editorInfo.timeout);
         setTimeout(() => {
             editorInfo.text = editorInfo.editor.getValue();
-            let newProgram = twgl.createProgramInfo(gl, ["vs", headerShader + editorInfo.text], (err) => {
-                console.log(err);
+            let fragShaderString = headerShader + editorInfo.text;
+            let newProgram = twgl.createProgramInfo(gl, ["vs", fragShaderString], (err) => {
+                setMarkersForShaderError(editorInfo, err);
+                // if(debugging) console.log(err);
             });
+            if(newProgram) editorInfo.editor.session.clearAnnotations();
             if (editorIndex === 1 && newProgram) {
                 programInfo = newProgram;
             }
--- template/drawing.js
+++ template/drawing.js
@@ -1,5 +1,5 @@
 console.log("drawing.js eval");
 
 function drawing(){
-    ellipse(p5w/2, p5h/2, min(p5w/2, p5h/2)/2);
+    ellipse(p5w/2, p5h/2 + sin(time)*100, min(p5w/2, p5h/2)/2);
 }
